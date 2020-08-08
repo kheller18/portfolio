007a2c4cbfa3f07abaf79aa1143dc3634d1be1bf
Keenan Heller <keenanheller@MacBook-Pro.hsd1.wa.comcast.net>
2020-08-08 15:46:12 -0700

    updated design of front page and other sections.

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,22 @@
 $(document).ready(function() {
 
+    let nav_icon = $(".nav-right-icon");
 
+    // if (!(nav_icon)) {
+
+    // }
+
+    $(".nav-right-icon").on("click", function() {
+        $(".nav-right").css("display", "block");
+    })
+
+    $(".nav-right").on("click", function(event) {
+        console.log(event);
+        let link = event.target.id;
+        link = link.slice(4);
+        console.log(link);
+        window.location = `${ link }.html`;
+        // event.preventDefault();
+        // console.log(window.location);
+    });
 });
